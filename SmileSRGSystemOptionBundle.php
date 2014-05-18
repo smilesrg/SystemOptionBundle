ac706be79b2a8f21cdec4a8afd6ded2bfbf8cb1b
Sergey Smirnov <smilesrg@gmail.com>
2014-05-19 00:32:35 +0300

    Added compiler passes for Doctrine models

--- SmileSRGSystemOptionBundle.php
+++ SmileSRGSystemOptionBundle.php
@@ -3,7 +3,40 @@
 namespace SmileSRG\SystemOptionBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+use Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass;
+use Doctrine\Bundle\MongoDBBundle\DependencyInjection\Compiler\DoctrineMongoDBMappingsPass;
 
 class SmileSRGSystemOptionBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $modelDir = realpath(__DIR__.'/Resources/config/doctrine/model');
+        $mappings = array(
+            $modelDir => 'SmileSRG\SystemOptionBundle\Model',
+        );
+
+        $ormCompilerClass = 'Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass';
+        if (class_exists($ormCompilerClass)) {
+            $container->addCompilerPass(
+                DoctrineOrmMappingsPass::createXmlMappingDriver(
+                    $mappings,
+                    array('system_option.model_manager'),
+                    'system_option.backend_type_orm'
+                ));
+        }
+
+        $mongoCompilerClass = 'Doctrine\Bundle\MongoDBBundle\DependencyInjection\Compiler\DoctrineMongoDBMappingsPass';
+        if (class_exists($mongoCompilerClass)) {
+            $container->addCompilerPass(
+                DoctrineMongoDBMappingsPass::createXmlMappingDriver(
+                    $mappings,
+                    array('system_option.model_manager'),
+                    'system_option.backend_type_mongodb'
+                ));
+        }
+    }
 }
